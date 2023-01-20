7da0747863a941fa9bb8f064c08c641c50a958cb
ShashankPole <pole.shashank@infokalash.com>
2023-01-20 16:32:56 +0530

    Testing Class updated

---
+++ src/com/hungry/Test2.java
@@ -0,0 +1,16 @@
+package com.hungry;
+
+public class Test2 {
+
+public static void  main(String[] args) {
+		
+		String hungry =  "Yes";
+		if(hungry == "yes") {
+			System.out.println("Go and eat");
+		}
+		else {
+			System.out.println("go and work");
+		}
+	}
+	
+}
