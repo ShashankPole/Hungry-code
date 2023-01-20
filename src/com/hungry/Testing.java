e56cb02b4925abe30d0ee1121cae6dac59814bdb
ShashankPole <pole.shashank@infokalash.com>
2023-01-20 16:27:02 +0530

    Hungry code sent

---
+++ src/com/hungry/Testing.java
@@ -0,0 +1,16 @@
+package com.hungry;
+
+public class Testing {
+
+	
+	public static void  main(String[] args) {
+		
+		String hungry =  "Yes";
+		if(hungry == "yes") {
+			System.out.println("Go and eat");
+		}
+		else {
+			System.out.println("go and work");
+		}
+	}
+}
